8ab513536b1051e3e1068bcd9ebc4e4c32f3c6b3
haarooo <pshb200137@gmail.com>
2026-02-15 22:00:40 +0900

    practice15 미완

--- src/day13/Practice15.java
+++ src/day13/Practice15.java
@@ -3,14 +3,32 @@ package day13;
 import 도서관리.model.BookDto.BookDto;
 import 종합예제7.model.dto.BoardDto;
 
+import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Practice15 {
     static void main() {
 
+        //1
+        BookDto1 day = new BookDto1();
+        BoardDto1 day1 = new BoardDto1();
+        System.out.println(day);
+        System.out.println(day1);
 
+        //2
+        String str1 = new String("유재석");
+        String str2 = new String("유재석");
+        System.out.println( str1 == str2 );
+        System.out.println( str1.equals( str2 ));
 
+        //3
+        String str3 = new String("강호동");
+        Class c1 = str3.getClass();
+        try { Class c2 = Class.forName("java.lang.String");
+        }catch (ClassNotFoundException e){}
+        Method[ ] methods = c1.getMethods();
+        int count = methods.length;
 
 
 
@@ -32,4 +50,14 @@ public class Practice15 {
 
     }//main e
 }//class e
+
+class BookDto1{
+
+}
+class BoardDto1{
+    @Override
+    public String toString() {
+        return "BoardDto1{}";
+    }
+}
 
